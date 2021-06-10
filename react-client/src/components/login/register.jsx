6d547175c3384c7ee85a539470a25890549b257b
DESKTOP-CFR740R\USER <milkmanchusa@gmail.com>
2021-06-10 13:53:47 +0700

    Add login fb and google

--- react-client/src/components/login/register.jsx
+++ react-client/src/components/login/register.jsx
@@ -2,50 +2,50 @@ import React from "react";
 import loginImg from "../../login.svg";
 
 export class Register extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="base-container">
-                <div className="header">Sign Up</div>
-                <div className="content"> 
-                    <div className="image">
-                        <img scr={loginImg} />
-                    </div>
-                    <div className="form">
-                        <div className="form-group">
-                            <label htmlFor="Email"></label>
-                            <input type="text" name="Email" placeholder="Email"/>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password"></label>
-                        <input type="password" name="Password" placeholder="Password"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password"></label>
-                        <input type="password" name="Confirm Password" placeholder="Confirm Password"/>
-                    </div>
-                </div>    
-            <div className="footer">
-                <button type="button" className="btn">
-                    Sign Up
-                </button>
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Sign Up</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="Email"></label>
+              <input type="text" name="Email" placeholder="Email" />
             </div>
-            <div className="or">-------------------- or --------------------</div>   
-            <div className="footer">
-                <button type="button" className="btn">
-                    Sign up with Facebook
-                </button>
+            <div className="form-group">
+              <label htmlFor="Password"></label>
+              <input type="password" name="Password" placeholder="Password" />
             </div>
-            <div className="footer">
-                <button type="button" className="btn">
-                    Sign up with Google
-                </button>
+            <div className="form-group">
+              <label htmlFor="Confirm Password"></label>
+              <input type="password" name="Confirm Password" placeholder="Confirm Password" />
             </div>
-            </div>
-        );
-    }
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Sign up
+          </button>
+        </div>
+        <div className="footer">-------------------- or --------------------</div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Sign in with Facebook
+          </button>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Sign in with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
