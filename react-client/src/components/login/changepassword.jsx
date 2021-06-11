9e34f3ae28ce9d8ea53b9b34b2413051f192b152
DESKTOP-CFR740R\USER <milkmanchusa@gmail.com>
2021-06-11 15:57:35 +0700

    Add navbar not finish

--- react-client/src/components/login/changepassword.jsx
+++ react-client/src/components/login/changepassword.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import loginImg from "../../login.svg";
 
-export class Login extends React.Component {
+export class ForgotPassword extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -9,11 +8,8 @@ export class Login extends React.Component {
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">SIGN IN</div>
+        <div className="header">FORGET PASSWORD</div>
         <div className="content">
-          <div className="image">
-            <img src={loginImg} />
-          </div>
           <div className="form">
             <div className="form-group">
               <label htmlFor="Email"></label>
